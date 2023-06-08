09f32561ecbfff5a4f85c4522407927b334f7bd3
Filip Keri <fkeri@fandm.edu>
2023-06-08 12:26:33 +0100

    Try removing pd.read_json for render.com

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from dash import Dash
 from dash_bootstrap_components.themes import LUX
 
 from src.components.layout import create_layout
-import data_utils
 
 import pandas as pd
+import json
 
-BILLBOARD_SPOTIFY_FILEPATH = "data\\processed\\billboardYearlySpotifyTracksAudioFeatures.json"
+BILLBOARD_SPOTIFY_FILEPATH = (
+    "data\\processed\\billboardYearlySpotifyTracksAudioFeatures.json"
+)
 ANIMATION_FRAMES_PATH = "data\\processed\\billboardAnimationFramesDecade.json"
 BILLBOARD_HISTORICAL_1960_1990_PATH = "data\\external\\1960-1990_yearData.json"
 BILLBOARD_HISTORICAL_1991_2022_PATH = "data\\external\\1991-2022_yearData.json"
@@ -15,26 +17,32 @@ YEAR_END = 2022
 
 
 def load_billboard_hot_100_data(filepath_1, filepath_2):
-    file_1 = pd.read_json(filepath_1)
-    file_2 = pd.read_json(filepath_2)
+    with open(filepath_1, "r") as json_file:
+        file_1 = json.load(json_file)
+        file_1 = pd.DataFrame(file_1)
+    with open(filepath_2, "r") as json_file:
+        file_2 = json.load(json_file)
+        file_2 = pd.DataFrame(file_2)
     return pd.concat([file_1, file_2]).reset_index(drop=True)
+
 
 app = Dash(external_stylesheets=[LUX])
 server = app.server
 
-# Load the data
-data = pd.read_json(BILLBOARD_SPOTIFY_FILEPATH)
-# This is a {'year': DataFrame} Dictionary for years in [1960, 2022]
+# Load Billboard-Spotify Data
+with open(BILLBOARD_SPOTIFY_FILEPATH, "r") as json_file:
+    json_data = json.load(json_file)
+data = pd.DataFrame(json_data)
+
+# Load weekly raw Billboard Chart Data
 data_billboard_charts = load_billboard_hot_100_data(
     BILLBOARD_HISTORICAL_1960_1990_PATH, BILLBOARD_HISTORICAL_1991_2022_PATH
 )
 
-# Let's concatenate the above Dictionary Dataframes:
-# data_billboard_charts = pd.concat(
-#     [billboard_charts_df[year] for year in range(YEAR_START, YEAR_END + 1)]
-# ).reset_index(drop=True)
-
-data_animation = pd.read_json(ANIMATION_FRAMES_PATH)
+# Load animation frames for animated contour plots
+with open(ANIMATION_FRAMES_PATH, "r") as json_file:
+    json_data = json.load(json_file)
+data_animation = pd.DataFrame(json_data)
 data_animation["duration_ms"] /= 1000 * 60
 data_animation.rename(columns={"duration_ms": "duration_min"}, inplace=True)
 
